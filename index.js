64eae2e19f0351c964c5e92108515794b14738fe
Zheng Jie Ng <zhengjie.zjie@gmail.com>
2025-04-19 07:56:33 +0800

    Add website bg, top buttons and animated greetings

---
+++ index.js
@@ -0,0 +1,26 @@
+let user = "Zheng Jie";
+
+const d = new Date();
+let hour = d.getHours();
+
+let greeting = "Good evening";
+
+if (hour >= 6 && hour < 12) {
+  greeting = "Good morning";
+} else if (hour >= 12 && hour < 18) {
+  greeting = "Good afternoon";
+}
+
+document.getElementsByClassName(
+  "message"
+)[0].innerHTML = `${greeting}, ${user}!`;
+
+$(document).ready(function () {
+  $("h1").animate(
+    {
+      height: "-=10vh",
+      opacity: 0.9,
+    },
+    "slow"
+  );
+});
